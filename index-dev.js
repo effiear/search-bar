222a16e44c3468d719dfdf07ab2af08f7c333a0e
effiear <effiea@wix.com>
2018-09-12 16:45:50 +0300

    logs

--- index-dev.js
+++ index-dev.js
@@ -1,18 +1,19 @@
 const { app, greynode, emitConfigs } = require('./dist/test/environment');
 
-console.log('step1');
+console.log('index-dev.js --> Starting app...');
+console.log('step1 - Starting greynode...');
 greynode
   .start()
   .then(() => {
-    console.log('step2');
+    console.log('step2 - Greynode started, emitting config...');
     return greynode.emitConfig();
   })
   .then(() => {
-    console.log('step3');
+    console.log('step3 - Config emitted...');
     return emitConfigs();
   })
   .then(() => {
-    console.log('step4');
+    console.log('step4 - Starting App...');
     return app.start();
   })
   .then(() => {
